1539238f1d719f2a4ccfe76804ee99c6be493bf5
Kris Gray <kag56@cam.ac.uk>
2026-07-17 14:29:59 +0100

    fix(orm): export core family models from models.core package (Task T4)

--- src/my_g4public_orm/models/core/__init__.py
+++ src/my_g4public_orm/models/core/__init__.py
@@ -1,1 +1,15 @@
-"""Core models module."""
+"""Core model exports for ``my_g4public_orm.models.core``."""
+
+from my_g4public_orm.models.core.family_alias import FamilyAlias
+from my_g4public_orm.models.core.family_new import FamilyNew
+from my_g4public_orm.models.core.hierarchy import Hierarchy
+from my_g4public_orm.models.core.hierarchy_closure import HierarchyClosure
+from my_g4public_orm.models.core.pub_hgnc import PubHgnc
+
+__all__ = [
+    "FamilyAlias",
+    "FamilyNew",
+    "Hierarchy",
+    "HierarchyClosure",
+    "PubHgnc",
+]
